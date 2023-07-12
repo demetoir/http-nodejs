70721039c106bc45b625819494b55ea461e99cf4
demetoir <wnsqlehlswk@naver.com>
2023-07-12 15:51:38 +0900

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: 'fast',
-            // script: './fastify.main.js',
+            script: './fastify.main.js',
             // name: 'hyper-express',
             // script: './hyper-express.main.js',
             instances: 1,
